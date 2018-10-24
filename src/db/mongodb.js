51270b77efc56d9bbe42e42c0477663465d08cd7
Hamelina <ehamelojulia@gmail.com>
2018-10-24 15:30:56 +0200

    Change env variable access

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 //TODO: change the URL to env variables
 mongoose.connect(process.env.MONGO_URL/*'mongodb://localhost:27017'*/, { /*promiseLibrary: require('bluebird'),*/ useNewUrlParser: true})
   .then(() => console.log('Connection to database succesful'))
-  .catch((err) => console.error(err));
+  .catch((err) => {
+      console.error(err);
+      console.log(process.env.MONGO_URL);
+    });
 
 /*mongoClient.connect('mongodb://localhost:27017', function(err, client){
     console.log("Connected succedfully");
