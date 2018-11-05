bffa8d4468b86c1db80c48ed09c7ef354df3c5f0
assilelyahyaoui <elyahyaouiassil@hotmail.com>
2018-11-05 10:17:11 +0100

    boardController

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -120,3 +120,49 @@ module.exports = {
       ))
   )
 }
+//TOD:
+/**
+ const boardModel = require('../models');
+ const requestError = require('../util/errorHandler');
+* find board by date of last update
+*/
+
+/**
+* find lists of a board
+*/
+
+/**
+* find by users
+*/
+
+/**
+* find public board 
+*/
+
+/*const boardControler = {
+    create: function (data)  {
+        try{
+            const board = new Board(
+                data.description,
+                data.name,
+                data.idMembers,
+                data.idTeams,
+                data.isPublic,
+                data.owners,
+                data.isClosed);
+            board.save;
+                return board;
+            }
+            catch (err) {
+                //check if error of field or server error
+                if(err.status){
+                    throw err;
+                }
+                throw new requestError(500, 'Internal server error');
+                
+            }
+        }, 
+
+
+
+}*/
