b62084c14c7f960d6e1f5ff041cb0f82da2cd15b
assilelyahyaoui <elyahyaouiassil@hotmail.com>
2018-11-14 12:52:18 +0100

    dispatcher action type : addlisttoboard

--- src/socket/dispatcher.js
+++ src/socket/dispatcher.js
@@ -1,8 +1,17 @@
 const ADD_LIST_TO_BOARD = "@@board/ADD_LIST_TO_BOARD";
+const listController = require('../controllers/list'); 
 
 const dispatcher = (action, user) => new Promise((resolve, reject) => {
   switch(action.type){
     case ADD_LIST_TO_BOARD:
+    console.log('dispatcher', action)
+    const idBoard = action.payload.idBoard; 
+    let createdList = action.payload ; 
+
+    listController.createInBoard({ idBoard, createdList }, user)
+    .then(list => console.log(list))
+    .catch(error => reject('error dispatcher '+ action.type + error))
+
     default:
       return reject('Unhandled action: ' + JSON.stringify(action));
   }
