e9617bc07b3a33d0824cffb8d9c0facadc3eba3f
Fournier Clement <fournier.clt@gmail.com>
2018-11-17 16:30:16 +0100

    [SOCKET] Dispatch must resolve

--- src/socket/dispatcher.js
+++ src/socket/dispatcher.js
@@ -14,139 +14,95 @@ const ASSIGN_CHECKLIST_TO_CARD = "@@card/ASSIGN_CHECKLIST_TO_CARD ";
 const ADD_LABEL_TO_CARD = "@@label/ADD_LABEL_TO_CARD";
 
 
-const listController = require('../controllers/list'); 
-const cardController = require('../controllers/card'); 
-const labelController = require('../controllers/label'); 
+const listController = require('../controllers/list');
+const cardController = require('../controllers/card');
+const labelController = require('../controllers/label');
 
 
 const dispatcher = (action, user) => new Promise((resolve, reject) => {
-  switch(action.type){
+  switch (action.type) {
     // ----------------- ADDING --------------------
     // -----------------Add List to board -----------------
     case ADD_LIST_TO_BOARD:
-    var idBoard = action.payload.idBoard; 
-    let createdList = action.payload ; 
+      var idBoard = action.payload.idBoard;
+      let createdList = action.payload;
 
-    return listController.createInBoard({ idBoard, createdList }, user)
-    .then(list => console.log(list))
-    .catch(error => reject('error dispatcher '+ action.type + error))
+      return listController.createInBoard({ idBoard, createdList }, user).then(resolve, reject);
 
     // -----------------Add Card to list -----------------
     case ADD_CARD_TO_LIST:
-    var idList= action.payload.idList; 
-    let createdCard = action.payload ; 
+      var idList = action.payload.idList;
+      let createdCard = action.payload;
 
-    return cardController.createInList ({ idList, createdCard })
-    .catch(error => reject('error dispatcher '+ action.type + error))
-    break; 
+      return cardController.createInList({ idList, createdCard }).then(resolve, reject);
 
-     // -----------------Add Label to Card -----------------
-     case ADD_LABEL_TO_CARD:
-     var idCard = action.payload.idCard; 
-     var createdLabel = action.payload ; 
- 
-     return labelController.createInCard ({ idCard, createdLabel })
-     .then(card => console.log(card))
-     .catch(error => reject('error dispatcher '+ action.type + error))
-      break;
+    // -----------------Add Label to Card -----------------
+    case ADD_LABEL_TO_CARD:
+      var idCard = action.payload.idCard;
+      var createdLabel = action.payload;
+
+      return labelController.createInCard({ idCard, createdLabel }).then(resolve, reject);
     // -----------------UPDATE CARD ACTIONS -----------------
     case SET_CARD_NAME:
-    var idCard= action.payload.id; 
-    var newValue = action.payload.name ; 
+      var idCard = action.payload.id;
+      var newValue = action.payload.name;
 
-    return   cardController.updateCardName ({ idCard, newValue })
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
+      return cardController.updateCardName({ idCard, newValue }).then(resolve, reject);
     // -----------------Set card desc-----------------
 
     case SET_CARD_DESC:
-      var idCard= action.payload.id; 
-      var newValue = action.payload.desc ; 
-      return   cardController.updateCardDesc({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
+      var idCard = action.payload.id;
+      var newValue = action.payload.desc;
+      return cardController.updateCardDesc({ idCard, newValue }).then(resolve, reject);
     // -----------------Set card due date-----------------
 
     case SET_CARD_DUE_DATE:
-    console.log('SET CARD DUE DATE ', action)
-      var idCard= action.payload.id; 
-      var newValue = action.payload.dueDate ; 
+      console.log('SET CARD DUE DATE ', action)
+      var idCard = action.payload.id;
+      var newValue = action.payload.dueDate;
 
-      return  cardController.updateCardDuedate ({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
-  // -----------------Set card all day-----------------
+      return cardController.updateCardDuedate({ idCard, newValue }).then(resolve, reject);
+    // -----------------Set card all day-----------------
 
-  case SET_CARD_ALL_DAY:
-  console.log('SET CARD ALL DAY ', action)
-    var idCard= action.payload.id; 
-    var newValue = action.payload.allDay ; 
+    case SET_CARD_ALL_DAY:
+      console.log('SET CARD ALL DAY ', action)
+      var idCard = action.payload.id;
+      var newValue = action.payload.allDay;
 
-    return  cardController.updateCardAllDay({ idCard, newValue })
-    .then(card => console.log(card))
-    .catch(error => reject('error dispatcher '+ action.type + error))
-    break; 
-     // -----------------Set card position-----------------
+      return cardController.updateCardAllDay({ idCard, newValue }).then(resolve, reject);
+    // -----------------Set card position-----------------
 
     case SET_CARD_POSITION:
-    console.log('SET CARD POSITION', action)
-      var idCard= action.payload.id; 
-      var newValue = action.payload.position ; 
+      console.log('SET CARD POSITION', action)
+      var idCard = action.payload.id;
+      var newValue = action.payload.position;
 
-      return cardController.updateCardPosition ({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
-     // -----------------SET CARD CLOSED -----------------
+      return cardController.updateCardPosition({ idCard, newValue }).then(resolve, reject);
+    // -----------------SET CARD CLOSED -----------------
 
     case SET_CARD_CLOSED:
-    console.log('SET NAME TO CARD', action)
-      var idCard= action.payload.id; 
-      var newValue = action.payload.closed ; 
-      return cardController.updateCardClosed ({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
+      console.log('SET NAME TO CARD', action)
+      var idCard = action.payload.id;
+      var newValue = action.payload.closed;
+      return cardController.updateCardClosed({ idCard, newValue }).then(resolve, reject);
 
-      // -----------------SET CARD DUECOMPLETE -----------------
+    // -----------------SET CARD DUECOMPLETE -----------------
 
-      case SET_CARD_DUE_COMPLETE:
-    console.log('SET CARD DUE COMPLETE', action)
-      var idCard= action.payload.id; 
-      var newValue = action.payload.closed ; 
-      return cardController.updateCardDueComplete ({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
-      break; 
-
+    case SET_CARD_DUE_COMPLETE:
+      console.log('SET CARD DUE COMPLETE', action)
+      var idCard = action.payload.id;
+      var newValue = action.payload.closed;
+      return cardController.updateCardDueComplete({ idCard, newValue }).then(resolve, reject);
 
     case SET_CARD_LIST:
-      console.log('SET NAME TO CARD', action)
-      const idCard= action.payload.id; 
-      const newValue = action.payload.isClosed ; 
-
-      cardController.updateCardClosed ({ idCard, newValue })
-      .then(card => console.log(card))
-      .catch(error => reject('error dispatcher '+ action.type + error))
     case ASSIGN_LABEL_TO_CARD:
     case ASSIGN_CHECKLIST_TO_CARD:
     case ASSIGN_MEMBER_TO_CARD:
 
-
-    
-
-
-
-
-
-
-
     // ----------------- DEFAULT --------------------
 
     default:
-      return reject('Unhandllllled action: ' + JSON.stringify(action));
+      return reject('Unhandled action: ' + JSON.stringify(action));
   }
 });
 
